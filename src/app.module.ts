50e020542221d22951b825f16e1b08adf65226ca
arthur.furtunato <arthur.furtunato@lais.huol.ufrn.br>
2022-06-30 20:39:55 -0300

    .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,32 @@ import { UsersModule } from './users/users.module';
 import { ReportModule } from './report/report.module';
 import { User } from './users/users.entity';
 import { Report } from './report/report.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [User, Report],
+        };
+      },
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [User, Report],
+    //   synchronize: true,
+    // }),
     UsersModule,
     ReportModule,
   ],
